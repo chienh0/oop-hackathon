4d58baae07a03cfc078c2cd8f8fb900a60c85db3
Shlok Natarajan <shlok.natarajan@gmail.com>
2024-06-22 16:32:47 -0700

    feat: initial commit

---
+++ deepscribe-testing/deepscribe_demo.py
@@ -0,0 +1,55 @@
+import requests
+import time
+from api_keys import X_API_KEY
+
+# Upload 
+base_url = "https://api.deepscribe.ai/partners/v1/note"
+
+payload = { "format": "m4a" }
+upload_headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "X-API-KEY": X_API_KEY
+}
+
+response = requests.post(base_url, json=payload, headers=upload_headers).json()
+noteId = response["noteId"]
+upload_url = response["audioUploadUrl"]
+file_path = "audio_files/ems_test_audio_2.m4a"
+def upload_audio_file(url, file_path):
+    """
+    Uploads an audio file to the specified URL using a PUT request.
+
+    :param url: The URL to which the file will be uploaded
+    :param file_path: The path to the audio file to upload
+    :return: The response from the server
+    """
+    try:
+        with open(file_path, 'rb') as file:
+            response = requests.put(url, data=file)
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            return response
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+    
+response = upload_audio_file(upload_url, file_path)
+print(response.text)
+# need the note id 
+time.sleep(60)
+print("--- Waiting 60 seconds ---")
+# GET the data
+get_url = f"https://api.deepscribe.ai/partners/v1/note/{noteId}"
+
+get_headers = {
+    "accept": "application/json",
+    "X-API-KEY": X_API_KEY
+}
+
+response = requests.get(get_url, headers=get_headers)
+
+print(response.text)
+
+
+
+
